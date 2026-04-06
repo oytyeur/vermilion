59992c6c53dd8f79956dae5a12dceec3142d1d89
oytyeur <khkhlov.ai@mail.ru>
2026-04-06 18:29:44 +0300

    Add: follower logic and sportclient control

--- src/perception/video/detection/specific_object_follower/src/specific_object_follower.cpp
+++ src/perception/video/detection/specific_object_follower/src/specific_object_follower.cpp
@@ -1,7 +1,8 @@
 #include "../include/specific_object_follower.hpp"
 
 SpecificObjectFollower::SpecificObjectFollower(const rclcpp::NodeOptions& options) :
-    rclcpp::Node("specific_object_follower_node", options) {
+    rclcpp::Node("specific_object_follower_node", options),
+    sport_client_(this) {
 
     this->detection_sub_ = 
         this->create_subscription<Detection2DArr>("input",
@@ -10,16 +11,52 @@ SpecificObjectFollower::SpecificObjectFollower(const rclcpp::NodeOptions& option
                                                             this,
                                                             std::placeholders::_1));
     
-    this->image_pub_ = 
-        this->create_publisher<Image>("output",
-                                      rclcpp::QoS{1});
+    // this->image_pub_ = 
+    //     this->create_publisher<Image>("output",
+    //                                   rclcpp::QoS{1});
+
+
 
     this->conf_threshold = 
         this->declare_parameter("conf_threshold", 0.8);
+    this->spec_obj_name = 
+        this->declare_parameter("spec_obj_name", "redball");
 }
 
 void SpecificObjectFollower::detectionArrayCallback(const Detection2DArr::ConstSharedPtr msg) {
     std::vector<Detection2D> det_arr = msg->detections;
+    
+    bool has_valid_obj = false;
+    double w = 0.0;
+    double x = -1.0;
+    
+    if (det_arr.empty()) {
+        this->sport_client_.StopMove(this->req_);
+    } else {
+        Detection2D valid_detection;
+        HypoWithPose hypo_and_pose;
+        hypo_and_pose.hypothesis.score = 0.0;
+        valid_detection.results.push_back(hypo_and_pose);
+        
+        for (const auto& det : det_arr) {
+            const auto object_hypo = det.results.at(0).hypothesis;
+            if (object_hypo.class_id != this->spec_obj_name or object_hypo.score < this->conf_threshold) {
+                continue;
+            }
+    
+            if (object_hypo.score > valid_detection.results.at(0).hypothesis.score) {
+                valid_detection = det;
+                x = valid_detection.bbox.center.position.x;
+                // err = this->CENTRE_X - x;
+                w = (this->CENTRE_X - x) * this->Kp;
+            }
+        }
+    }
+    if (!has_valid_obj) {
+        this->sport_client_.StopMove(this->req_);
+    } else {
+        this->sport_client_.Move(this->req_, 0.0, 0.0, w);
+    }
 
-    RCLCPP_INFO(this->get_logger(), "%li", det_arr.size());
+    RCLCPP_INFO(this->get_logger(), "X: %f, W: %f", x, w);
 }
